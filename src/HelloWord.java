559a80601ba2a003f20aee85d0a7b2418f26affa
xiayouwen <3294779373@qq.com>
2019-12-06 15:02:59 +0800

    第一次提交

---
+++ src/HelloWord.java
@@ -0,0 +1,13 @@
+/**
+ * @Auther:
+ * @Date: 2019/12/5 *
+ * @Description: PACKAGE_NAME *
+ * @version: 1.0
+ */
+public class HelloWord {
+
+    public static void main(String[] args){
+        System.out.println("HelloWord");
+    }
+
+}
